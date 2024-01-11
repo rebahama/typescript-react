18a0c3053e68ed34c3157572fd9c33226655c8ac
rebahama <wiri@hotmail.se>
2024-01-11 19:06:00 +0000

    add navbar

--- socialtype/src/components/NavBar.tsx
+++ socialtype/src/components/NavBar.tsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const NavBar = () => {
+  const [hello, sethello] = useState<string | null>("testing");
+
+  const testing = () => {
+    sethello("new value");
+  };
+
   return (
     <div className="navbar">
-    <ul>
-     <li>Home</li>
-     <li>Blog</li>
-    </ul>
-   </div>
-  )
-}
+      <ul>
+        <li>Home</li>
+        <li>Blog</li>
+        <li>{hello}</li>
+        <button onClick={testing}> click here </button>
+      </ul>
+    </div>
+  );
+};
 
-export default NavBar
+export default NavBar;
