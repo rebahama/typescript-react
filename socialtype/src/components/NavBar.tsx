9398e8225a8417937fbbbec605b4156abc13ee68
rebahama <wiri@hotmail.se>
2024-01-11 18:32:39 +0000

    create components folder

---
+++ socialtype/src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+    <ul>
+     <li>Home</li>
+     <li>Blog</li>
+    </ul>
+   </div>
+  )
+}
+
+export default NavBar
