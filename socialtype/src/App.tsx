2a2908a61fff6e7a805fbc46f558db06981312b0
rebahama <97362222+rebahama@users.noreply.github.com>
2024-02-01 06:11:14 +0100

    add feat(TS) create routing for page

--- socialtype/src/App.tsx
+++ socialtype/src/App.tsx
@@ -2,12 +2,16 @@ import NavBar from './components/NavBar';
 import './styles/style.scss';
 import './api/axiosDefault';
 import HomePage from './pages/HomePage';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <NavBar />
-      <HomePage />
+      <Routes>
+      <Route path="typescript-react/" element={<HomePage />} />
+      <Route path="typescript-react/home" element="hello" />
+      </Routes>
     </div>
   );
 }
